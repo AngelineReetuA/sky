240177eac14c7ef5a16a3432e12dfac3d8c1127b
AngelineReetuA <angelinereetu@gmail.com>
2025-10-19 18:17:46 +0530

    initial commit of sky

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import SkyBase from "./components/sky-base";
+
+export default function App() {
+  return <SkyBase />;
+}
---
+++ src/components/sky-base.tsx
@@ -0,0 +1,14 @@
+import "./sky-base.css";
+
+const SkyBase = () => {
+  return (
+    <div className="sky-container">
+      <div className="cloud cloud1"></div>
+      <div className="cloud cloud2"></div>
+      <div className="cloud cloud3"></div>
+      <div className="ground"></div>
+    </div>
+  );
+};
+
+export default SkyBase;
